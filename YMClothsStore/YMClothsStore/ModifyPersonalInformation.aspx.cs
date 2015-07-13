a214918caa1604587beb3d5908329ea6b5d2a2ca
BrunoQin <brunoqin@163.com>
2015-07-13 10:54:56 +0800

    test

---
+++ YMClothsStore/YMClothsStore/ModifyPersonalInformation.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace YMClothsStore
+{
+
+    public partial class ModifyPersonalInformation : System.Web.UI.Page
+    {
+        //根据员工ID号修改个人信息
+        //首先需要获得该员工ID信息
+        //根据员工ID在数据库中查询所有的信息
+        //员工输入要更改的信息
+        //如果没有输入就按照原来的填写（页面中的空格应该有原本信息）
+        //update到原本数据库中
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void getStaff_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+    }
+}
