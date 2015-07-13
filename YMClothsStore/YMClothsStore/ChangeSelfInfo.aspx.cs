2166c8fec11fe228318aaa9cc95e880ca0973875
Liu xuodng <7lxd@tongji.edu.cn>
2015-07-14 00:08:18 +0800

    修改个人信息后端接口添加

--- YMClothsStore/YMClothsStore/ChangeSelfInfo.aspx.cs
+++ YMClothsStore/YMClothsStore/ChangeSelfInfo.aspx.cs
@@ -9,8 +9,34 @@ namespace YMClothsStore
 {
     public partial class ChangeSelfInfo : System.Web.UI.Page
     {
+        // TODO
+        protected Staff theStaff = DBModel.sharedDBModel().findStaffInformationById("null now");
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+        protected void modifyEmployeesInfo()
+        {
+            // get new info from view 
+            string employeeId = Request.Form["EmployeeId"];
+            string employeeName = Request.Form["EmployeeName"];
+            string employeePhone = Request.Form["EmployeePhone"];
+            string employeePwd = Request.Form["EmployeePwd"];
+            string employeeJob = Request.Form["EmployeeJob"];
+            string employeeGender = Request.Form["EmployeeGender"];
+            int employeeShopId =int.Parse(Request.Form["EmployeeShopId"]);
+            Staff updateStaff = new Staff();
+            // set new info 
+            updateStaff.staffLogId = theStaff.staffLogId;
+            updateStaff.staffId =  theStaff.staffId;
+            updateStaff.staffName = employeeName;
+            updateStaff.staffPhone = employeePhone;
+            updateStaff.password = employeePwd;
+            updateStaff.staffJob = employeeJob;
+            updateStaff.staffGender = employeeGender;
+            updateStaff.shopId = employeeShopId;
+            // update database 
+            DBModel.sharedDBModel().modifyPersonalInformation(updateStaff);
 
         }
     }
