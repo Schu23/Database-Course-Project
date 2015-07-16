d4212e3ec72351d849c602abdbceab0b96439db5
Liu xuodng <7lxd@tongji.edu.cn>
2015-07-16 22:06:36 +0800

    add new view  liu

---
+++ YMClothsStore/YMClothsStore/Manager_ClothesInfo.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace YMClothsStore
+{
+    public partial class Manager_ClothesInfo : System.Web.UI.Page
+    {
+        protected staff theStaff;
+        protected item[] searchResult;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            theStaff = (staff)Session["Staff"];
+            searchResult = DBModel.sharedDBModel().getAllItemsOfThisShop(theStaff.staffId);
+        }
+
+        protected void SerachSubmit(object sender, EventArgs e)
+        {
+            string condition = Request.Form["searchCondition"];
+            string serachKey = Request.Form["searchKey"];
+            System.Diagnostics.Debug.WriteLine("serach key and serach condition test :" + serachKey + condition);
+
+            if (condition.Equals("staffName"))
+            {
+                searchResult = DBModel.sharedDBModel().getItemByItemName(serachKey);
+            }
+            else
+                searchResult = DBModel.sharedDBModel().getItemByItemId(serachKey);
+        }
+    }
+}
